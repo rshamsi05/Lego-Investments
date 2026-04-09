2ab920a1682a583c1a3cee9b84cca3fa38e951aa
Rayan Shamsi <rayanshamsi@Rayans-MacBook-Pro.local>
2026-04-09 16:23:07 -0700

    Createdstaging sets script and added staging SQL file to rename column names.

--- transform/models/staging/stg_sets.sql
+++ transform/models/staging/stg_sets.sql
@@ -1,1 +1,13 @@
 -- Cleans and standardizes raw sets table from Rebrickable
+-- NOTE: Staging sets python file already renames the columns, so this SQL script is not needed as of now, we can refactor later.
+SELECT set_num AS set_id, name, year,
+    -- Casting theme_id as a string for consistency
+    CAST(theme_id AS STRING) as theme_id,
+
+    -- Rename num_parts to piece_count
+    num_parts as piece_count,
+
+    -- Add metadata
+    CURRENT_TIMESTAMP() AS ingested_at
+-- Read from the raw staging table
+FROM 'lego-investments.lego_staging.stg_sets';
