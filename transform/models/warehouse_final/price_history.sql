d37c7c15c922a5414cea86282d8d5d63603ed4cd
Rayan Shamsi <rayanshamsi@Rayans-MacBook-Pro.local>
2026-04-21 15:51:31 -0700

    Refactored transform moduel with staging sets and staging prices filea, and created price_history staging table.

--- transform/models/warehouse_final/price_history.sql
+++ transform/models/warehouse_final/price_history.sql
@@ -1,1 +1,24 @@
 -- Full cleaned price history for every set over time.
+-- What does this mean
+{{ config(materialized='table') }}
+
+-- How exactly does this sql code segment work?
+WITH basic_prices AS (
+    SELECT *
+    FROM {{ source('lego_staging', 'stg_prices')}}
+    WHERE avg_price_usd IS NOT NULL
+)
+
+SELECT 
+    set_id,
+    observed_date,
+    avg_price_usd,
+    --- How does the window function work here?
+    AVG(avg_price_usd) OVER (
+        PARTITION BY set_id
+        ORDER BY UNIX_DATE(observed_date)
+        ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
+    ) AS price_30d_avg_usd,
+    source,
+    CURRENT_TIMESTAMP() AS last_updated
+FROM basic_prices
